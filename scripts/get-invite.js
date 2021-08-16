3bbe1ecf42aa86dcefb5f97053b3b9da527cc1ea
ignis05 <48099798+ignis05@users.noreply.github.com>
2021-08-16 15:07:58 +0200

    feat: save client id when using get-invite

--- scripts/get-invite.js
+++ scripts/get-invite.js
@@ -7,10 +7,13 @@ const { auth } = require('../modules/config')
 const client = new Client({ intents: [] })
 
 client.once('ready', () => {
+	// save client id for register script
+	writeFileSync('./data/clientId.json', JSON.stringify({ clientId: client.user.id }))
+	// invite link
 	let invite = client.generateInvite({ scopes: ['applications.commands', 'bot'], permissions: ['SEND_MESSAGES', 'EMBED_LINKS'] })
 	writeFileSync('./data/invite.txt', invite)
 	console.log(`Generated invite link and saved it in data/invite.txt:\n${invite}`)
-  exit(0)
+	exit(0)
 })
 
 client.login(auth.token)
